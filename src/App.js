2b272321bb8ad71602a2f0d6f16458d11d745c67
Shubham Singh <shbh29@gmail.com>
2023-05-20 14:40:13 +0530

    Functional Componenet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
 
-// const HelloWorld = <h1> Hello, Pesto Babes! </h1>;
 
-class HelloWorld extends React.Component {
-  render() {
-    return <h1> Hello World! </h1>;
-  }
-    
+function HelloWorld() {
+  return <h1> Hello, Function Component! </h1>;
 }
+
 
 export { HelloWorld };
