ed0a012da013c5141be0cfb6e067eb65b481f5ad
Shubham Singh <shbh29@gmail.com>
2023-05-20 15:20:31 +0530

    functionality to button added!

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 class Welcome extends React.Component {
   render () {
     const {value} = this.props;
-    return <h1> Hello, {value}</h1>;
+    return (
+      <>
+        <h1> Hello, {value}</h1>
+        <button onClick={function() { console.log(`button was clicked! ${value}`)}}>Click</button>
+      </>
+    )
+    
   }
 }
 
