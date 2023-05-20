2fd602dd4e5558da334c11ba1c296b8d256eb553
Shubham Singh <shbh29@gmail.com>
2023-05-20 14:34:50 +0530

    React Componenets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import './App.css';
 import React from 'react';
+import './App.css';
 
-const HelloWorld = <h1> Hello, Pesto Babes! </h1>;
+// const HelloWorld = <h1> Hello, Pesto Babes! </h1>;
 
-export default HelloWorld;
+class HelloWorld extends React.Component {
+  render() {
+    return <h1> Hello World! </h1>;
+  }
+    
+}
+
+export { HelloWorld };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import HelloWorld  from './App';
+import { HelloWorld } from './App';
 
 const rootEl = document.getElementById('root');
 
 
-ReactDOM.render(HelloWorld, rootEl);
+ReactDOM.render(<HelloWorld />, rootEl);
